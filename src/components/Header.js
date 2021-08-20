ded7e815475199eb5128b97026cd2885df9224d0
faizy4ktech <faiz@webfabricant.com>
2021-08-20 16:07:11 +0500

    new
    new change

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,27 +13,7 @@ const Header = () => {
                                     <div className='col header-input-wrapper'> 
                                           <div className='col-sm-12 d-flex justify-content-center align-items-center' style={{ marginBottom: '200px' }}>
                                                 
-                                                {/* <form action="">
-                                                      
-                                                      <div className="row header-content-div">
-                                                            <div className="col-sm-5">
-                                                                  <div class="input-group mb-3 header-input">
-                                                                        <span class="input-group-text"><i class="fa fa-pencil fa-fw" aria-hidden="true"></i></span>
-                                                                        <input type="text" className="form-control jobtitle" placeholder="Job Title" aria-label="Job Title" />
-                                                                  </div>
-                                                            </div>
-                                                            <div className="col-sm-5">
-                                                                  <div class="input-group mb-3">
-                                                                        <span class="input-group-text"><i class="fa fa-map-marker fa-fw" aria-hidden="true"></i></span>
-                                                                        <input type="text" className="form-control location" placeholder="Location" aria-label="Location" />
-                                                                  </div>
-                                                            </div>
-                                                            <div className="col searchbtn-div">
-                                                                  <button type="submit" className="btn searchbtn" style={{ backgroundColor: '#067d1f', color: 'white' }}>Search</button>
-                                                            </div>
-                                                      </div>
-
-                                                </form> */}
+                                                
                                                 
                                                 
                                                
@@ -46,8 +26,8 @@ const Header = () => {
                                                       <form className="p-2 header-form" style={{ background: 'white', borderRadius: 5 }}>
                                                             <div className="sub-input-wrapper d-flex flex-row justify-content-between align-items-center">
                                                                    
-                                                                  <input type="text" className="form-control m-2" placeholder="&#xf002; &nbsp; Job Title" style={{fontFamily: 'FontAwesome'}}/>
-                                                                  <input type="text" className="form-control m-2" placeholder="&#xf041; &nbsp; Location" style={{fontFamily: 'FontAwesome'}}/>
+                                                                  <input type="text" className="form-control border-0 m-2" placeholder="&#xf002; &nbsp; Job Title"/>
+                                                                  <input type="text" className="form-control border-0 m-2" placeholder="&#xf041; &nbsp; Location"/>
                                                                     
                                                                         <button type="submit" className="btn btn-success m-2">Search</button>
                                                               
--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -13,14 +13,14 @@ const Subscribe = () => {
                                     <p style={{ color: 'white' }}>Get notification about tips, new clothes and exclusive promo news just for you.</p>
 
                                     <div className="row justify-content-center">
-                                          <div className='col-sm-4 mb-5' style={{ marginTop: '40px' }}>
+                                          <div className='col-sm-4 col-md-6 col-lg-4 mb-5' style={{ marginTop: '40px' }}>
 
 
 
                                                 <form action="" style={{background: 'white', borderRadius:5}}>
                                                       <div className="d-flex flex-row justify-content-between align-items-center">
                                                                    
-                                                                  <input type="email" className="form-control m-2" placeholder="&#xf0e0; &nbsp; Enter you email address" style={{fontFamily: 'FontAwesome'}}/>
+                                                                  <input type="email" className="form-control border-0 m-2" placeholder="&#xf0e0; &nbsp; Enter you email address" style={{fontFamily: 'FontAwesome'}}/>
 
                                                                   <button type="submit" className="btn subscribebtn m-2" style={{ backgroundColor: '#067d1f', color: 'white' }}>Subscribe</button>
 
