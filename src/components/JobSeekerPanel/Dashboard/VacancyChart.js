c99506307246470fe1b84d80d10983487ee3ea4b
ali-hassan1-122 <ah.alihassan09@gmail.com>
2021-09-04 13:57:02 +0500

    new
    new

--- src/components/JobSeekerPanel/Dashboard/VacancyChart.js
+++ src/components/JobSeekerPanel/Dashboard/VacancyChart.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import { Line } from 'react-chartjs-2';
 const VacancyChart = () => {
 
     return (
         <>
             <div className="top-response d-flex flex-row justify-content-between align-items-center">
-                <h6 className="text-white">Vacancy Stats</h6> 
+                <h6 className="text-white">Vacancy Stats</h6>
                 <div className="chart-wrapper">
                     <ul className="nav nav-pills p-0 mb-3 rounded-pill" id="pills-tab" role="tablist">
                         <li className="nav-item" role="presentation">
@@ -22,15 +22,249 @@ const VacancyChart = () => {
             </div>
 
 
-            <div className="tab-content text-white" id="pills-tabContent">
-                <div className="tab-pane fade show active" id="pills-daily-vacancy" role="tabpanel" aria-labelledby="pills-daily-vacancy-tab">
-                    daily vacancy chart...
+            <div className="tab-content" id="pills-tabContent">
+                <div style={{ height: "380px" }} className="tab-pane fade show active" id="pills-daily-vacancy" role="tabpanel" aria-labelledby="pills-daily-vacancy-tab">
+
+                    <Line
+                        data={{
+                            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov ', 'Dec'],
+                            datasets: [
+                                {
+                                    label: "Application Sent",
+                                    data: [10, 20, 30, 40, 50, 60, 70, 4, 3, 5, 76, 40],
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)',
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+
+                                    ],
+                                    borderColor: [
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)',
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                    ],
+                                    borderWidth: 1
+                                },
+                            ],
+
+                        }}
+
+                        height={20}
+                        width={50}
+                        options={{
+                            responsive: true,
+                            maintainAspectRatio: false,
+                            plugins: {
+                                legend: {
+                                    labels: {
+                                        color: "white",
+
+                                        font: {
+                                            size: 12
+                                        }
+                                    }
+                                }
+                            },
+
+
+                            scales: {
+                                y: {
+                                    ticks: {
+                                        color: "white",
+
+                                        font: {
+                                            size: 10,
+                                        },
+                                        // stepSize: 1,
+                                        beginAtZero: true
+                                    }
+                                },
+                                x: {
+                                    ticks: {
+                                        color: "white",
+
+                                        font: {
+                                            size: 10,
+                                        },
+                                        // stepSize: 1,
+                                        beginAtZero: true
+                                    }
+                                },
+                            }
+                        }}
+
+                    />
                 </div>
-                <div className="tab-pane fade" id="pills-weekly-vacancy" role="tabpanel" aria-labelledby="pills-weekly-vacancy-tab">
-                    weekly vacancy chart..
+                <div style={{ height: "380px" }} className="tab-pane fade" id="pills-weekly-vacancy" role="tabpanel" aria-labelledby="pills-weekly-vacancy-tab">
+                    <Line
+                        data={{
+                            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov ', 'Dec'],
+                            datasets: [
+                                {
+                                    label: "Application Sent",
+
+                                    data: [10, 20, 30, 40, 60, 55, 40, 4, 3, 5, 56, 20],
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)',
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+
+                                    ],
+                                    borderColor: [
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)',
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                    ],
+                                    borderWidth: 1
+                                },
+                            ],
+
+                        }}
+
+                        height={20}
+                        width={50}
+                        options={{
+                            responsive: true,
+                            maintainAspectRatio: false,
+                            plugins: {
+                                legend: {
+                                    labels: {
+                                        color: "white",
+
+                                        font: {
+                                            size: 12
+                                        }
+                                    }
+                                }
+                            },
+                            scales: {
+                                y: {
+                                    ticks: {
+                                        color: "white",
+
+                                        font: {
+                                            size: 10,
+                                        },
+                                        // stepSize: 1,
+                                        beginAtZero: true
+                                    }
+                                },
+                                x: {
+                                    ticks: {
+                                        color: "white",
+
+                                        font: {
+                                            size: 10,
+                                        },
+                                        // stepSize: 1,
+                                        beginAtZero: true
+                                    }
+                                },
+                            }
+                        }}
+
+                    />
                 </div>
-                <div className="tab-pane fade" id="pills-monthly-vacancy" role="tabpanel" aria-labelledby="pills-monthly-vacancy-tab">
-                    monthly vacancy chart...
+                <div style={{ height: "380px" }} className="tab-pane fade" id="pills-monthly-vacancy" role="tabpanel" aria-labelledby="pills-monthly-vacancy-tab">
+                    <Line
+                        data={{
+                            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov ', 'Dec'],
+                            datasets: [
+                                {
+                                    label: "Application Sent",
+
+                                    data: [10, 20, 30, 40, 53, 330, 70, 4, 3, 5, 76, 40],
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)',
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+
+                                    ],
+                                    borderColor: [
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)',
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                    ],
+                                    borderWidth: 1
+                                },
+                            ],
+
+                        }}
+
+                        height={20}
+                        width={50}
+                        options={{
+                            responsive: true,
+                            maintainAspectRatio: false,
+                            plugins: {
+                                legend: {
+                                    labels: {
+                                        color: "white",
+
+                                        font: {
+                                            size: 12
+                                        }
+                                    }
+                                }
+                            },
+                            scales: {
+                                y: {
+                                    ticks: {
+                                        color: "white",
+
+                                        font: {
+                                            size: 10,
+                                        },
+                                        // stepSize: 1,
+                                        beginAtZero: true
+                                    }
+                                },
+                                x: {
+                                    ticks: {
+                                        color: "white",
+
+                                        font: {
+                                            size: 10,
+                                        },
+                                        // stepSize: 1,
+                                        beginAtZero: true
+                                    }
+                                },
+                            }
+
+                        }}
+
+                    />
                 </div>
             </div>
         </>
--- src/components/User/Dashboard/ExpenseChart.js
+++ src/components/User/Dashboard/ExpenseChart.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import { Bar } from 'react-chartjs-2';
 const ExpenseChart = () => {
 
     return (
         <>
             <div className="top-response d-flex flex-row justify-content-between align-items-center">
-                <h6>Expense Stats</h6> 
+                <h6>Expense Stats</h6>
                 <div className="chart-wrapper">
                     <ul className="nav nav-pills mb-3 rounded-pill" id="pills-tab" role="tablist">
                         <li className="nav-item " role="presentation">
@@ -20,12 +20,149 @@ const ExpenseChart = () => {
                     </ul>
                 </div>
             </div>
-           
+
 
             <div className="tab-content" id="pills-tabContent">
-                <div className="tab-pane fade show active" id="pills-daily-expense" role="tabpanel" aria-labelledby="pills-daily-expense-tab">daily expense chart...</div>
-                <div className="tab-pane fade" id="pills-weekly-expense" role="tabpanel" aria-labelledby="pills-weekly-expense-tab">weekly expense chart..</div>
-                <div className="tab-pane fade" id="pills-monthly-expense" role="tabpanel" aria-labelledby="pills-monthly-expense-tab">monthly  chart...</div>
+                <div style={{ height: "380px" }} className="tab-pane fade show active" id="pills-daily-expense" role="tabpanel" aria-labelledby="pills-daily-expense-tab">
+                    <Bar
+                        data={{
+                            labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13'],
+                            datasets: [
+                                {
+                                    label: "Delivered",
+                                    data: [10, 20, 30, 40, 50, 60, 4, 6, 23, 56, 38, 2, 41, 29, 7],
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)',
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                    ],
+                                    borderColor: [
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)',
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                    ],
+                                    borderWidth: 1
+                                },
+                            ],
+
+                        }}
+
+                        height={20}
+                        width={50}
+                        options={{
+                            responsive: true,
+                            maintainAspectRatio: false,
+
+                        }}
+
+                    />
+                </div>
+                <div style={{ height: "380px" }} className="tab-pane fade" id="pills-weekly-expense" role="tabpanel" aria-labelledby="pills-weekly-expense-tab">
+                    <Bar
+                        data={{
+                            labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11'],
+                            datasets: [
+                                {
+                                    label: "Delivered",
+
+                                    data: [10, 20, 30, 40, 50, 60, 70, 4, 3, 5, 76],
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)',
+                                        'rgba(255, 99, 132, 0.2)',
+
+
+                                    ],
+                                    borderColor: [
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)',
+                                        'rgba(255, 99, 132, 1)',
+
+                                    ],
+                                    borderWidth: 1
+                                },
+                            ],
+
+                        }}
+
+                        height={20}
+                        width={50}
+                        options={{
+                            responsive: true,
+                            maintainAspectRatio: false,
+
+                        }}
+
+                    />
+                </div>
+                <div style={{ height: "380px" }} className="tab-pane fade" id="pills-monthly-expense" role="tabpanel" aria-labelledby="pills-monthly-expense-tab">
+                    <Bar
+                        data={{
+                            labels: ['01', '02', '03', '04', '05', '06', '07'],
+                            datasets: [
+                                {
+                                    label: "Delivered",
+                                    data: [10, 20, 30, 40, 50, 60, 70],
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)'
+                                    ],
+                                    borderColor: [
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)'
+                                    ],
+                                    borderWidth: 1
+                                },
+                            ],
+
+                        }}
+
+                        height={20}
+                        width={50}
+                        options={{
+                            responsive: true,
+                            maintainAspectRatio: false,
+
+                        }}
+
+                    />
+                </div>
             </div>
         </>
     );
