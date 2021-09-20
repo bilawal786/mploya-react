66762790e865b5ace4478dc5c5a7dda019ea87d3
faizy4ktech <faiz@webfabricant.com>
2021-09-20 14:38:26 +0500

    new
    .new

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,110 +1,110 @@
 import React from 'react';
-import { NavLink } from "react-router-dom"; 
+import { NavLink } from "react-router-dom";
 
 
 const Categories = () => {
 
-      return (
-            <>
+    return (
+        <>
             <div className='container my-container'>
                 <div className='section-title'>
                     <h4>Explore By
-                    <span style={{color:"#067d1f"}}> Categories</span>
-                    </h4> 
+                        <span style={{ color: "#067d1f" }}> Categories</span>
+                    </h4>
                     <div className="row">
-                        <div className= "col-sm-8">
+                        <div className="col-sm-8">
                             <p>Explore the different types of categories job available to rentand discover which is right for your speciality.</p>
                         </div>
-                        <div className= "col-sm-4"> 
+                        <div className="col-sm-4">
                             <NavLink className="btn btn-success" to="/jobs">See More <i class="fa fa-arrow-right" aria-hidden="true"></i></NavLink>
                         </div>
                     </div>
-                     
-                </div> 
+
+                </div>
                 <br></br>
                 <div className='row'>
                     <div className="col-sm-6 col-md-4 col-lg-3 my-3 mx-auto">
-                    <div className="card shadow-lg card-styled"> 
-                        <div className="card-body"> 
-                        <div class="badge badge-green"> <span><i className="fa fa-desktop"></i></span> </div> 
-                        <h5 className="card-title my-3"><b>Design & <br/> Development</b></h5>
-                        <p className="card-text text-muted">15K <span>Post Jobs</span></p> 
+                        <div className="card shadow-lg card-styled">
+                            <div className="card-body">
+                                <div class="badge badge-green"> <span><i className="fa fa-desktop"></i></span> </div>
+                                <h5 className="card-title my-3"><b>Design & <br /> Development</b></h5>
+                                <p className="card-text text-muted">15K <span>Post Jobs</span></p>
+                            </div>
                         </div>
                     </div>
-                    </div>  
-                    <div className="col-sm-4 col-md-4 col-lg-3 my-3">
-                    <div className="card shadow-lg card-styled"> 
-                        <div className="card-body"> 
-                        <div class="badge badge-red"> <span><i className="fa fa-desktop"></i></span> </div>                         
-                        <h5 className="card-title my-3"><b>Working & <br/> Translation</b></h5>
-                        <p className="card-text text-muted">15K <span>Post Jobs</span></p> 
+                    <div className="col-sm-4 col-md-4 col-lg-3 my-3 mx-auto">
+                        <div className="card shadow-lg card-styled">
+                            <div className="card-body">
+                                <div class="badge badge-red"> <span><i className="fa fa-desktop"></i></span> </div>
+                                <h5 className="card-title my-3"><b>Working & <br /> Translation</b></h5>
+                                <p className="card-text text-muted">15K <span>Post Jobs</span></p>
+                            </div>
                         </div>
                     </div>
-                    </div>  
-                    <div className="col-sm-4 col-md-4 col-lg-3 my-3">
-                    <div className="card shadow-lg card-styled"> 
-                        <div className="card-body"> 
-                        <div class="badge badge-yellow"> <span><i className="fa fa-desktop"></i></span> </div>                         
-                        <h5 className="card-title my-3"><b>Marketing & <br/> Communication</b></h5>
-                        <p className="card-text text-muted">15K <span>Post Jobs</span></p> 
+                    <div className="col-sm-4 col-md-4 col-lg-3 my-3 mx-auto">
+                        <div className="card shadow-lg card-styled">
+                            <div className="card-body">
+                                <div class="badge badge-yellow"> <span><i className="fa fa-desktop"></i></span> </div>
+                                <h5 className="card-title my-3"><b>Marketing & <br /> Communication</b></h5>
+                                <p className="card-text text-muted">15K <span>Post Jobs</span></p>
+                            </div>
                         </div>
                     </div>
-                    </div>  
-                    <div className="col-sm-4 col-md-4 col-lg-3 my-3">
-                    <div className="card shadow-lg card-styled"> 
-                        <div className="card-body"> 
-                        <div class="badge badge-red"> <span><i className="fa fa-desktop"></i></span> </div>                         
-                        <h5 className="card-title my-3"><b>Project & <br/> Management</b></h5>
-                        <p className="card-text text-muted">15K <span>Post Jobs</span></p> 
+                    <div className="col-sm-4 col-md-4 col-lg-3 my-3 mx-auto">
+                        <div className="card shadow-lg card-styled">
+                            <div className="card-body">
+                                <div class="badge badge-red"> <span><i className="fa fa-desktop"></i></span> </div>
+                                <h5 className="card-title my-3"><b>Project & <br /> Management</b></h5>
+                                <p className="card-text text-muted">15K <span>Post Jobs</span></p>
+                            </div>
                         </div>
                     </div>
-                    </div>  
-                    <div className="col-sm-4 col-md-4 col-lg-3 my-3">
-                    <div className="card shadow-lg card-styled"> 
-                        <div className="card-body"> 
-                        <div class="badge badge-red"> <span><i className="fa fa-desktop"></i></span> </div> 
-                        <h5 className="card-title my-3"><b>Buisness & <br/> Development</b></h5>
-                        <p className="card-text text-muted">15K <span>Post Jobs</span></p> 
+                    <div className="col-sm-4 col-md-4 col-lg-3 my-3 mx-auto">
+                        <div className="card shadow-lg card-styled">
+                            <div className="card-body">
+                                <div class="badge badge-red"> <span><i className="fa fa-desktop"></i></span> </div>
+                                <h5 className="card-title my-3"><b>Buisness & <br /> Development</b></h5>
+                                <p className="card-text text-muted">15K <span>Post Jobs</span></p>
+                            </div>
                         </div>
                     </div>
-                    </div>  
-                    <div className="col-sm-4 col-md-4 col-lg-3 my-3">
-                    <div className="card shadow-lg card-styled"> 
-                        <div className="card-body"> 
-                        <div class="badge badge-green"> <span><i className="fa fa-desktop"></i></span> </div>                         
-                        <h5 className="card-title my-3"><b>Design & <br/> Development</b></h5>
-                        <p className="card-text text-muted">15K <span>Post Jobs</span></p> 
+                    <div className="col-sm-4 col-md-4 col-lg-3 my-3 mx-auto">
+                        <div className="card shadow-lg card-styled">
+                            <div className="card-body">
+                                <div class="badge badge-green"> <span><i className="fa fa-desktop"></i></span> </div>
+                                <h5 className="card-title my-3"><b>Design & <br /> Development</b></h5>
+                                <p className="card-text text-muted">15K <span>Post Jobs</span></p>
+                            </div>
                         </div>
                     </div>
-                    </div>  
-                    <div className="col-sm-4 col-md-4 col-lg-3 my-3">
-                    <div className="card shadow-lg card-styled"> 
-                        <div className="card-body"> 
-                        <div class="badge badge-red"> <span><i className="fa fa-desktop"></i></span> </div>                         
-                        <h5 className="card-title my-3"><b>Design & <br/> Development</b></h5>
-                        <p className="card-text text-muted">15K <span>Post Jobs</span></p> 
+                    <div className="col-sm-4 col-md-4 col-lg-3 my-3 mx-auto">
+                        <div className="card shadow-lg card-styled">
+                            <div className="card-body">
+                                <div class="badge badge-red"> <span><i className="fa fa-desktop"></i></span> </div>
+                                <h5 className="card-title my-3"><b>Design & <br /> Development</b></h5>
+                                <p className="card-text text-muted">15K <span>Post Jobs</span></p>
+                            </div>
                         </div>
                     </div>
-                    </div>  
-                    <div className="col-sm-4 col-md-4 col-lg-3 my-3">
-                    <div className="card shadow-lg card-styled"> 
-                        <div className="card-body"> 
-                        <div class="badge badge-yellow"> <span><i className="fa fa-desktop"></i></span> </div> 
-                        <h5 className="card-title my-3"><b>Customer & <br/> Support</b></h5>
-                        <p className="card-text text-muted">15K <span>Post Jobs</span></p> 
+                    <div className="col-sm-4 col-md-4 col-lg-3 my-3 mx-auto">
+                        <div className="card shadow-lg card-styled">
+                            <div className="card-body">
+                                <div class="badge badge-yellow"> <span><i className="fa fa-desktop"></i></span> </div>
+                                <h5 className="card-title my-3"><b>Customer & <br /> Support</b></h5>
+                                <p className="card-text text-muted">15K <span>Post Jobs</span></p>
+                            </div>
                         </div>
                     </div>
-                    </div>  
-                    
-                    
+
+
                 </div>
-                
-                
-                
+
+
+
             </div>
-                 
-                 
-            </>
-      );
+
+
+        </>
+    );
 }
 export default Categories;
--- src/components/LatestJobs.js
+++ src/components/LatestJobs.js
@@ -24,7 +24,7 @@ const LatestJobs = () => {
                 </div>
                 <br></br>
                 <div className='row'>
-                    <div className="col-md-4 my-3">
+                    <div className="col-sm-6 col-md-6 col-lg-4 my-3">
                         <div className="card p-3 shadow-lg card-post">
                             <div className="d-flex justify-content-between align-items-center">
                                 <div className="d-flex flex-row align-items-center">
@@ -52,34 +52,7 @@ const LatestJobs = () => {
 
                         </div>
                     </div>
-                    <div className="col-md-4 my-3">
-                        <div className="card p-3 shadow-lg card-post">
-                            <div className="d-flex justify-content-between align-items-center">
-                                <div className="d-flex flex-row align-items-center">
-                                    <img class="icon-img" src="https://i.imgur.com/IRsUTtE.jpg" alt="image" />
-                                    <div className="c-details px-2">
-                                        <h6 className="mb-0">Mailchimp</h6> <span>2 min ago</span>
-                                    </div>
-
-                                </div>
-                                <button class="btn btn-light btn-circle btn-circle-sm m-1"><i class="fa fa-bell"></i></button>
-                            </div>
-                            <h5 className="card-title my-3"><b>Data Entry Staff</b></h5>
-                            <div className="c-details">
-                                <span><i class="fas fa-clock"></i>&nbsp;Full-Time</span>
-                                <span className='mx-3'><i class="fa fa-map" aria-hidden="true"></i> Full-Time</span>
-
-
-                            </div>
-                            <div className="c-details my-2">
-                                <span className='badge-type'>Design</span>
-                                <span className='badge-type mx-3'>UI/UX</span>
-
-                                <a className="section-link" href="#" style={{ float: 'right' }}><b>Apply</b></a>
-                            </div>
-
-                        </div>
-                    </div><div className="col-md-4 my-3">
+                    <div className="col-sm-6 col-md-6 col-lg-4 my-3">
                         <div className="card p-3 shadow-lg card-post">
                             <div className="d-flex justify-content-between align-items-center">
                                 <div className="d-flex flex-row align-items-center">
@@ -107,6 +80,35 @@ const LatestJobs = () => {
 
                         </div>
                     </div>
+                    <div className="col-sm-6 col-md-6 col-lg-4 my-3">
+                        <div className="card p-3 shadow-lg card-post">
+                            <div className="d-flex justify-content-between align-items-center">
+                                <div className="d-flex flex-row align-items-center">
+                                    <img class="icon-img" src="https://i.imgur.com/IRsUTtE.jpg" alt="image" />
+                                    <div className="c-details px-2">
+                                        <h6 className="mb-0">Mailchimp</h6> <span>2 min ago</span>
+                                    </div>
+
+                                </div>
+                                <button class="btn btn-light btn-circle btn-circle-sm m-1"><i class="fa fa-bell"></i></button>
+                            </div>
+                            <h5 className="card-title my-3"><b>Data Entry Staff</b></h5>
+                            <div className="c-details">
+                                <span><i class="fas fa-clock"></i>&nbsp;Full-Time</span>
+                                <span className='mx-3'><i class="fa fa-map" aria-hidden="true"></i> Full-Time</span>
+
+
+                            </div>
+                            <div className="c-details my-2">
+                                <span className='badge-type'>Design</span>
+                                <span className='badge-type mx-3'>UI/UX</span>
+
+                                <a className="section-link" href="#" style={{ float: 'right' }}><b>Apply</b></a>
+                            </div>
+
+                        </div>
+                    </div>
+                     
                 </div>
 
 
